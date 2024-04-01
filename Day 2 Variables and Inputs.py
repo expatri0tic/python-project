02a95596e23fe76ea1fcae96fe61afa84aae6507
Jon Snow <github@expatriotic.me>
2024-04-01 16:13:56 +0800

    more updates

--- Day 2 Variables and Inputs.py
+++ Day 2 Variables and Inputs.py
@@ -10,3 +10,16 @@ print("and are the ripe old age of")
 print(myAge)
 print("and clearly think that Replit is")
 print(replit)
+
+
+my_variable = input("WHO_GOES_THERE?")
+print(my_variable)
+
+
+myGrandma = input("How's your Grandma doing? 😘 ")
+print(myGrandma)
+
+myLunch = input("What are you having for lunch? ")
+print("Hmm, it sounds like you really should just order")
+print(myLunch)
+print("as soon as possible!")
