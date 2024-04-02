4676d2f7963bb96db0e78ed518b7b6df2180ce85
Jon Snow <github@expatriotic.me>
2024-04-02 23:22:23 +0800

    Day 3

---
+++ Day 3 Concatenation.py
@@ -0,0 +1,22 @@
+#Day 3 Concatenation
+print("=== Completely Serious Recipe ===")
+print("""You'll be asked a bunch of questions
+then we'll make you up an amazing meal""")
+print()
+
+food = input("A type of food: ")
+print()
+
+plant = input("A type of plant: ")
+print()
+
+method = input("A method of cooking: ")
+print()
+
+burned = input("A word to describe burned food: ")
+print()
+
+DIY = input("A household item: ")
+
+print()
+print(method, food, "with", burned, plant, "on a bed of", DIY)
