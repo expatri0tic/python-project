e2150b3429f6ac99ccd692e555c7ce3d42248c6d
Jon Snow <github@expatriotic.me>
2024-04-06 11:56:06 +0800

    Day 6 project

--- Day 6 if,else.py
+++ Day 6 if,else.py
@@ -1,6 +1,38 @@
+#My Project: Harry Potter
+gender = input("Are you a girl? ")
+if gender == "yes":
+  print("You are Hermionne Granger")
+else:
+  print("You are not Hermionne Granger")
+hair = input("Do you have red hair? ")
+if hair == "yes":
+  print("You are Ron Weasly")
+else:
+  print("You are not Ron Weasly")
+glasses = input("Do you wear glasses? ")
+if glasses == "yes":
+  print("You are Harry Potter")
+else:
+  print("You must be a hairy troll!!")
+
+#My name
 myName = input("What's your name?: ")
 if myName == "Jon":
   print("Welcome Handsome!")
   print("Want to come back to mine?")
 else:
   print("You're an ugly one aren't you?")
+
+#Cats or dogs
+  catsOrDogs = input("Are you a cat person? Or a dog person?: ")
+if catsOrDogs == "cat":
+  print("Meow")
+else:
+  print("Woof")
+
+#Drink choices
+  drink = input("Do you prefer coffee or tea?")
+if drink == "coffee":
+  print("Tea is better.")
+else:
+  print("Excellent choice.")
